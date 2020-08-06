9490f394107fcd335e28cfaa830a4103a78c1739
wonderfulkasey <kaseyarnold15@gmail.com>
2020-08-06 16:58:53 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,32 @@
 Rails.application.routes.draw do
 
+  root 'welcomes#home'
+
+  #
   resources :welcomes
+
+  resources :worlds do
+    resources :plots, :characters
+  end
+
   resources :plots
   resources :characters
-  resources :worlds
 
+  #
+  devise_for :users, controllers: {registrations: "registrations", 
+    omniauth_callbacks: "callbacks"}
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+    devise_scope :user do 
+      get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do 
+    get 'logout', to: 'devise/sessions#destroy'
+  end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
