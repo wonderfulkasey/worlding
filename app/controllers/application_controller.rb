d7a65c574d6435abb892298d5c7cc004a4d4d52d
wonderfulkasey <kaseyarnold15@gmail.com>
2020-08-19 11:48:46 -0400

    adding controller application index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
-
-   # before_action :authenticate_user!
+    before_action :authenticate_user!
+    
     #helper_method :set_world, :set_character, :set_plot
     helper_method :set_world, :set_character, :set_plot
 
+
     def home 
+        skip_before_action :authenticate_user!
     end 
 
     def index
-        before_action :authenticate_user!
-        before_action :set_world, except: [:index, :new, :create, :show, :destroy]
-        #@user = User.find_by(id: params[:id])
-        
+      #@user = User.find_by(id: params[:id])
+      
 
         if params[:id] == "my-worlds"
             redirect_to application_index_path
